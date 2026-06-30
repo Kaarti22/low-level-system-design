9a80e490f67cf57fac891efe0270dc5adb277e8d
Kaarti22 <kaartimondi@gmail.com>
2026-06-30 22:49:23 +0530

    aggregation relationship

---
+++ basics/src/main/java/com/sd/basics/UML/Aggregation.java
@@ -0,0 +1,47 @@
+package com.sd.basics.UML;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Professor {
+    private String name;
+
+    public Professor (String name) {
+        this.name = name;
+    }
+
+    public String getName () {
+        return name;
+    }
+}
+
+class Department {
+    private String name;
+    private List<Professor> professors;
+
+    public Department (String name, List<Professor> professors) {
+        this.name = name;
+        this.professors = professors;
+    }
+
+    public void showProfessors() {
+        System.out.println("Department: " + name);
+        for (Professor prof : professors) {
+            System.out.println(prof.getName());
+        }
+    }
+}
+
+public class Aggregation {
+
+    public static void main(String[] args) {
+        Professor prof1 = new Professor("Dr. Jones");
+        Professor prof2 = new Professor("Dr. Brown");
+
+        List<Professor> professors = Arrays.asList(prof1, prof2);
+        Department department = new Department("Computer Science", professors);
+
+        // aggregation relationship: department has professors, but professors exist independently
+        department.showProfessors();
+    }
+}
