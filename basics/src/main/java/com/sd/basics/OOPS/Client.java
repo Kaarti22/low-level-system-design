f16021d4a71b2b16cfeab8985017e66287c8423c
Kaarti22 <kaartimondi@gmail.com>
2026-06-30 21:51:41 +0530

    test new payment option using wallet

--- basics/src/main/java/com/sd/basics/OOPS/Client.java
+++ basics/src/main/java/com/sd/basics/OOPS/Client.java
@@ -8,8 +8,11 @@ public class Client {
         ps.addPaymentMethod("KartikeyaDebitCard", new DebitCard("1234", "Kartikeya"));
         ps.addPaymentMethod("KartikeyaCreditCard", new CreditCard("5678", "Kartikeya Mondi"));
         ps.addPaymentMethod("KartikeyaUPI", new UPI("kartikeya90"));
+        ps.addPaymentMethod("KartikeyaWallet", new Wallet());
 
         ps.makePayment("KartikeyaUPI");
         ps.makePayment("KartikeyaCreditCard");
+
+        ps.makePayment("KartikeyaWallet");
     }
 }
---
+++ basics/src/main/java/com/sd/basics/OOPS/Wallet.java
@@ -0,0 +1,9 @@
+package com.sd.basics.OOPS;
+
+public class Wallet implements PaymentMethod{
+
+    @Override
+    public void pay() {
+        System.out.println("Making payment via Wallet");
+    }
+}
